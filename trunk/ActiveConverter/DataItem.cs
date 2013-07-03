ed7f7defc2ed5dc5f55eb44308f22a565a6200bb
michal.koscak@gmail.com <michal.koscak@gmail.com@7a47c89a-3e8c-869e-df95-06983ed03b66>
2013-07-03 07:13:12 +0000

    Initial commit.

---
+++ trunk/ActiveConverter/DataItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ActiveConverter
+{
+    class DataItem
+    {
+        public DataItem()
+        {
+            Codes = new List<string>();
+            Sizes = new List<string>();
+        }
+
+        public List<string> Codes { get; set; }
+        public string Description { get; set; }
+        public string Rrp { get; set; }
+        public string Supp { get; set; }
+
+        public List<string> Sizes { get; set; }
+
+        public object Picture { get; set; }
+    }
+}
---
+++ trunk/ActiveConverter/FormMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ActiveConverter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 12);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(184, 62);
+            this.btnTest.TabIndex = 0;
+            this.btnTest.Text = "Process";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 99);
+            this.Controls.Add(this.btnTest);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Active Converter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
